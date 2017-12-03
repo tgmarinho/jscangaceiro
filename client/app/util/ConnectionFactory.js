4113eed1ef8fa6aa609cab0237457f5281098cf4
Thiago Marinho <tgmarinho@gmail.com>
2017-12-02 23:39:38 -0300

    Capítulo 15 - Colocando a casa em ordem

---
+++ client/app/util/ConnectionFactory.js
@@ -0,0 +1,81 @@
+const ConnectionFactory = (() => {
+
+    // array com uma store apenas
+    const stores = ['negociacaoes'];
+
+    // COMEÇA SEM CONEXÃO
+    let connection = null;
+
+    // VARIÁVEL QUE ARMAZENARÁ A FUNÇÃO ORIGINAL
+    let close = null;
+
+    return class ConnectionFactory {
+
+        constructor() {
+            throw new Error('Não é possível criar instâncias dessa classe');
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+
+                const openRequest = indexedDB.open('jscangaceiro', 2);
+
+                if (connection) return resolve(connection);
+
+                openRequest.onupgradeneeded = e => {
+
+                    // PASSA A CONEXÃO PARA O MÉTODO
+                    ConnectionFactory._createStores(e.target.result);
+
+                };
+
+                openRequest.onsuccess = e => {
+                    // SÓ SERÁ EXECUTADO NA PRIMEIRA VEZ QUE A CONEXÃO FOR CRIADA
+                    connection = e.target.result;
+
+                    // GUARDANDO A FUNÇÃO ORIGINAL!
+                    close = connection.close.bind(connection);
+
+                    connection.close = () => {
+                        throw new Error('Você não pode fechar diretamente a conexão');
+                    };
+                    resolve(e.target.result)
+                };
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error)
+                    reject(e.target.error.name)
+                };
+
+            });
+        }
+
+
+        // CONVENÇÃO DE MÉTODO PRIVADO
+        // SÓ FAZ SENTIDO SER CHAMADO PELA 
+        // PRÓPRIA CLASSE
+        static _createStores(connection) {
+
+            stores.forEach(store => {
+
+                // if sem bloco, mais sucinto!
+
+                if (connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
+
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+
+        static closeConnection() {
+            if (connection) {
+                // CHAMANDO O CLOSE ORIGINAL!
+                close();
+            }
+        }
+
+
+    }
+
+
+})();
