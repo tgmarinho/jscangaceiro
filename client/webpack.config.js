f649d468764f11b708ed354b3575ddfc47399b4d
Thiago Marinho <tgmarinho@gmail.com>
2017-12-07 18:28:07 -0300

    Capítulo 20 - 20.3 até 20.7

---
+++ client/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+// IMPORTANDO O PLUGIN
+const babiliPlugin = require('babili-webpack-plugin');
+
+// COMEÇA COM NENHUM PLUGIN
+let plugins = []
+
+if (process.env.NODE_ENV == 'production') {
+
+    // SE FOR PRODUCTION, ADICIONA O PLUGIN NA LISTA
+
+    plugins.push(new babiliPlugin());
+}
+
+module.exports = {
+    entry: './app-src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    // mesma coisa que plugins: plugins
+    plugins
+}
